a02db28af550e66012a8ff2e7d04eb8c598add88
Tom Jakubowski <tom@crystae.net>
2014-04-10 03:16:06 -0700

    do the handshake in net::connect()

--- src/restink/net.rs
+++ src/restink/net.rs
@@ -1,10 +1,13 @@
 use std::fmt;
 
-use std::io::IoResult;
+use std::io::{IoResult, IoError};
 use std::io::net::tcp::{TcpStream};
 use std::io::net::ip::{SocketAddr};
 
+static magic_number: i32 = 0x5e72273a;
+
 pub struct Connection {
+    // TODO: wrap in BufferedStream
     stream: TcpStream
 }
 
@@ -20,8 +23,50 @@ impl Drop for Connection {
     }
 }
 
+impl Connection {
+    // like read_to_end, but stops when a 0 is read.
+    fn read_to_null(&mut self) -> IoResult<~[u8]> {
+        let mut buf = ~[];
+        let mut x = try!(self.stream.read_byte());
+        while x != 0 {
+            buf.push(x);
+            x = try!(self.stream.read_byte());
+        }
+        Ok(buf)
+    }
+    fn write_handshake(&mut self) -> IoResult<()> {
+        // TODO: actually accept an optional authorization key
+        let api_key_len = 0;
+        try!(self.stream.write_le_i32(magic_number));
+        self.stream.write_le_i32(api_key_len)
+    }
+}
+
+fn other_io_error(desc: &'static str, detail: Option<~str>) -> IoError {
+    use std::io;
+    IoError { kind: io::OtherIoError, desc: desc, detail: detail }
+}
+
 pub fn connect(address: SocketAddr) -> IoResult<Connection> {
-    TcpStream::connect(address).and_then(|mut sock| {
-        sock.write_str("HELLO WORLD\n").and(Ok(Connection { stream: sock }))
-    })
+    use std::str;
+
+    let stream = try!(TcpStream::connect(address));
+    let mut conn = Connection { stream: stream };
+    try!(conn.write_handshake());
+    let response = try!(conn.read_to_null());
+    match str::from_utf8(response) {
+        Some("SUCCESS") => {
+            println!("connected + shook hands! :)");
+        },
+        Some(other) => {
+            let desc = "RethinkDB Handshake Error";
+            return Err(other_io_error(desc, Some(other.into_owned())));
+        },
+        None => {
+            let desc = "RethinkDB Handshake Error";
+            let detail = ~"couldn't read response as UTF-8 string";
+            return Err(other_io_error(desc, Some(detail)));
+        }
+    };
+    Ok(conn)
 }
