ab9e03606da999880a748f6011801862357207bd
Tom Jakubowski <tom@crystae.net>
2014-11-15 18:47:35 -0800

    handle JSON errors better (WIP)

--- src/errors.rs
+++ src/errors.rs
@@ -18,11 +18,12 @@ pub enum Error {
     RuntimeError(String),
     ProtocolError(String),
     DriverError(String), // FIXME: should this be merged with ProtocolError?
+    JsonParseError(json::ParserError),
     IoError(io::IoError)
 }
 
 impl Error {
-    pub fn new(kind: u8, res: Json) -> Error {
+    pub fn from_code_res(code: u8, res: Json) -> Error {
         let msgs = res.as_list();
         let msg = match msgs.map(|x| x.as_slice()) {
             Some([json::String(ref x)]) => x.to_string(),
@@ -31,15 +32,19 @@ impl Error {
             }
         };
 
-        match kind {
+        match code {
             CLIENT_ERROR => ClientError(msg),
             COMPILE_ERROR => CompileError(msg),
             RUNTIME_ERROR => RuntimeError(msg),
-            _ => ProtocolError(format!("unrecognized error number: {}", kind))
+            _ => ProtocolError(format!("unrecognized error number: {}", code))
         }
     }
 }
 
 impl FromError<io::IoError> for Error {
     fn from_error(e: io::IoError) -> Error { IoError(e) }
+}
+
+impl FromError<json::ParserError> for Error {
+    fn from_error(e: json::ParserError) -> Error { JsonParseError(e) }
 }
