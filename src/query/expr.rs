3a09b3d7e28d4a0d1b4ff7ec690ba13951d681f9
Tom Jakubowski <tom@crystae.net>
2014-11-23 18:04:31 -0800

    Add more binops to Datum

--- src/query/expr.rs
+++ src/query/expr.rs
@@ -13,8 +13,11 @@ impl<T> ToJson for Datum<T> {
     }
 }
 
+/// The ReQL NUMBER type.
 #[deriving(Clone)]
 pub struct Num;
+
+/// The ReQL STRING type.
 #[deriving(Clone)]
 pub struct String;
 
@@ -51,37 +54,64 @@ pub fn expr<D: ToDatum<T>, T>(x: D) -> Datum<T> {
     Datum(x.to_datum().to_json())
 }
 
-// We could impl Query here but Datum queries are weird and non-standard e.g.
-// for r::expr(420) the term is supposed to be just `1` on its own, not
-// something like [1, [420]], so the Query trait won't work without making the
-// common case annoying/wasteful (i.e. by making args() return Option<Vec>)
-// In practice this probably isn't a huge deal. Who wants to use RethinkDB as
-// the identity function anyway?
+// We would impl Query for Datum here but Datum queries are weird and not like
+// other queries e.g. for `r::expr(420i32)` the term is just `420` on its own,
+// not something like `[1, [420]]`, so the Query trait won't work. In practice
+// this probably isn't a huge deal. Who wants to use RethinkDB as the identity
+// function anyway?
 
 query! {
-    enum Add -> Json { // NOTE: we know it's Number -> Number or String -> String, so...
+    enum Add -> Json {
         Num { lhs: Datum<Num>, rhs: Datum<Num> },
         String { lhs: Datum<String>, rhs: Datum<String> }
     } ty::ADD
 }
 
-impl<T: ToDatum<Num>> ops::Add<T, Add> for Datum<Num> {
-    fn add(&self, rhs: &T) -> Add {
-        Add::Num {
-            lhs: self.clone(),
-            rhs: rhs.to_datum()
-        }
-    }
+// Several of the remaining query terms aren't polymorphic over ReQL types so
+// the enum might seem useless; purely for simplifying impl_datum_binop
+query! {
+    enum Sub -> Json {
+        Num { lhs: Datum<Num>, rhs: Datum<Num> }
+    } ty::SUB
 }
 
-impl<T: ToDatum<String>> ops::Add<T, Add> for Datum<String> {
-    fn add(&self, rhs: &T) -> Add {
-        Add::String {
-            lhs: self.clone(),
-            rhs: rhs.to_datum()
-        }
-    }
+query! {
+    enum Mul -> Json {
+        Num { lhs: Datum<Num>, rhs: Datum<Num> }
+    } ty::MUL
 }
+
+query! {
+    enum Div -> Json {
+        Num { lhs: Datum<Num>, rhs: Datum<Num> }
+    } ty::DIV
+}
+
+query! {
+    enum Rem -> Json {
+        Num { lhs: Datum<Num>, rhs: Datum<Num> }
+    } ty::MOD
+}
+
+macro_rules! impl_datum_binop {
+    ($op:ident / $fun:ident: $($ty:ident)|+) => {
+        $(impl<T: ToDatum<$ty>> ops::$op<T, $op> for Datum<$ty> {
+            fn $fun(&self, rhs: &T) -> $op {
+                $op::$ty {
+                    lhs: self.clone(),
+                    rhs: rhs.to_datum()
+                }
+            }
+        })+
+    };
+}
+
+// FIXME: should Add support `Array`s?
+impl_datum_binop! { Add / add: Num | String }
+impl_datum_binop! { Sub / sub: Num }
+impl_datum_binop! { Mul / mul: Num }
+impl_datum_binop! { Div / div: Num }
+impl_datum_binop! { Rem / rem: Num }
 
 #[cfg(test)]
 mod test {
@@ -99,5 +129,8 @@ mod test {
     fn test_ops() {
         assert_eq!((r::expr(420i32) + 123i32).to_json(), json!([24, [420, 123]]));
         assert_eq!((r::expr("foo") + "bar").to_json(), json!([24, ["foo", "bar"]]));
+        assert_eq!((r::expr(420i32) - 123i32).to_json(), json!([25, [420, 123]]));
+        assert_eq!((r::expr(420i32) * 123i32).to_json(), json!([26, [420, 123]]));
+        assert_eq!((r::expr(420i32) / 123i32).to_json(), json!([27, [420, 123]]));
     }
 }
--- src/query/term_type.rs
+++ src/query/term_type.rs
@@ -1,19 +1,27 @@
+// FIXME: generate this from ql2.proto
+
 pub type TermType = i64;
 
-pub const DB: TermType = 14;
+pub const DB   : TermType = 14;
 pub const TABLE: TermType = 15;
-pub const GET: TermType = 16;
+pub const GET  : TermType = 16;
 
 pub const ADD: TermType = 24;
+pub const SUB: TermType = 25;
+pub const MUL: TermType = 26;
+pub const DIV: TermType = 27;
+pub const MOD: TermType = 28;
 
 pub const DELETE: TermType = 54;
 pub const INSERT: TermType = 56;
-pub const DB_CREATE: TermType = 57;
-pub const DB_DROP: TermType = 58;
-pub const DB_LIST: TermType = 59;
+
+pub const DB_CREATE   : TermType = 57;
+pub const DB_DROP     : TermType = 58;
+pub const DB_LIST     : TermType = 59;
 pub const TABLE_CREATE: TermType = 60;
-pub const TABLE_DROP: TermType = 61;
-pub const TABLE_LIST: TermType = 62;
+pub const TABLE_DROP  : TermType = 61;
+pub const TABLE_LIST  : TermType = 62;
+
 pub const INDEX_CREATE: TermType = 75;
-pub const INDEX_DROP: TermType = 76;
-pub const INDEX_LIST: TermType = 77;
+pub const INDEX_DROP  : TermType = 76;
+pub const INDEX_LIST  : TermType = 77;
